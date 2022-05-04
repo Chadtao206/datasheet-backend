03094a6e05c5346465957e88293dc4d42a377d26
Chad Tao <chenchen408@gmail.com>
2022-05-04 16:07:23 -0700

    changes

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 const pdf = require("html-pdf");
-const graphqlHTTP = require("express-graphql");
+// const graphqlHTTP = require("express-graphql");
 const cors = require("cors");
-const path = require("path");
-const fs = require("fs");
-const axios = require("axios");
 const pdfTemplate = require("./templates/index");
 
 const app = express();
